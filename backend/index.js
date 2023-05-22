5c97e774227ca5a834c6ef764e1785172e4668fb
ihor-vip <vip20140902@gmail.com>
2023-05-22 14:08:06 +0300

    backend part, create server, connect to OPEN AI

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,33 @@
 import express from 'express';
-const port = 8080;
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
 
-app.get('/', (req, res) => res.send('Server is ready'));
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.get('/', async (req, res) => {
+    res.status(200).json({
+        message: 'Hello from DALL.E!',
+    });
+});
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('Server started on port 8080'));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
